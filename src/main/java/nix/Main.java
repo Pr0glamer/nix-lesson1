b72ab7ebea9a2f30741a150b834607401c21942c
Pr0glamer <nickolayladenko89@gmail.com>
2022-10-09 13:59:22 +0300

    added lucky number method

--- src/main/java/nix/Main.java
+++ src/main/java/nix/Main.java
@@ -1,5 +1,16 @@
-package nix;public class Main {
+package nix;
+
+import java.util.Random;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! From Mykola");
+        Main main = new Main();
+        main.printLuckyNumber();
     }
+
+    public void printLuckyNumber() {
+        System.out.println("Your lucky number is " + new Random().nextInt(10));
+    }
+
 }
